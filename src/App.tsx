e5478616249f9616fc3fe68c33762133fffb7e73
= <mateusz_pietruczuk07@wp.pl>
2023-09-19 09:10:58 +0200

    feat: create toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { MainLayout } from './layout/MainLayout';
 import { CategoriesList } from './components/hero/CategoriesList';
 import { ProductsList } from './components/productsList/ProductsList';
 import { ProductCard } from './components/productCard/ProductCard';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <MainLayout>
+      <Toaster/>
       <Routes>
         <Route path='/' element={<CategoriesList />} />
         <Route path='/:categoryName' element={<ProductsList />} />
