26427374b925fd2ff337ad8fcf311e43c8a4e0d0
Rafixx <rafal987.1@o2.pl>
2023-09-20 09:49:31 +0200

    feat: fetch categories from store

--- src/components/hero/CategoriesList.tsx
+++ src/components/hero/CategoriesList.tsx
@@ -1,42 +1,12 @@
 import { SimpleGrid } from '@chakra-ui/react';
-import { useAppSelector, useAppDispatch } from '../../redux/hooks';
-import {
-  addCategory,
-  deleteCategory,
-} from '../../redux/features/categories/categoriesSlice';
+import { useAppSelector } from '../../redux/hooks';
 import { CategoryItem } from './CategoryItem';
 
 export const CategoriesList = () => {
   const categories = useAppSelector((state) => state.categories);
-  const dispatch = useAppDispatch();
 
   return (
     <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4} justifyItems='center'>
-      <button
-        onClick={() => {
-          dispatch(
-            deleteCategory({
-              id: 7,
-            }),
-          );
-        }}
-      >
-        Delete this category of id 7
-      </button>
-      <button
-        onClick={() => {
-          dispatch(
-            addCategory({
-              id: 7,
-              categoryName: 'productivity',
-              backgroundColor: 'rgba(247,217,196,0.6)',
-              href: 'oomm',
-            }),
-          );
-        }}
-      >
-        Add new category
-      </button>
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
--- src/components/hero/CategoryItem.tsx
+++ src/components/hero/CategoryItem.tsx
@@ -1,83 +1,32 @@
 import { Link as RouterLink } from 'react-router-dom';
-import { useAppDispatch } from '../../redux/hooks';
-import {
-  deleteCategory,
-  editCategoryName,
-} from '../../redux/features/categories/categoriesSlice';
-import { Flex, Text, Center, Button, Box } from '@chakra-ui/react';
-import { type TCategories } from '../../types/categories';
-import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
+import { Flex, Text, Center } from '@chakra-ui/react';
+import { type TCategory } from '../../types/category';
 
-const isLogged: boolean = true;
-const isAdmin: boolean = true;
-
-export const CategoryItem = ({
-  category,
-}: {
-  category: Omit<TCategories, 'products'>;
-}) => {
-  const dispatch = useAppDispatch();
-
-  const deleteCategoryHandle = (id: number): void => {
-    dispatch(
-      deleteCategory({
-        id,
-      }),
-    );
-  };
-
-  const editCategoryHandle = () => {
-    dispatch(editCategoryName());
-  };
-
+export const CategoryItem = ({ category }: { category: TCategory }) => {
   return (
-    <>
-      {isLogged && isAdmin && (
-        <>
-          <Box>
-            <Button
-              colorScheme='teal'
-              variant='outline'
-              leftIcon={<DeleteIcon />}
-              onClick={() => deleteCategoryHandle(category.id)}
-              zIndex={999}
-            />
-          </Box>
-          {/* <Box>
-            <Button
-              colorScheme='teal'
-              variant='outline'
-              leftIcon={<EditIcon />}
-              onClick={() => deleteCategoryHandle(category.id)}
-              zIndex={999}
-            />
-          </Box> */}
-        </>
-      )}
-      <Flex
-        as={RouterLink}
-        to={`/${category.href}`}
-        maxW={{ base: '100%', md: '385px' }}
-        minW='100%'
-        h='203px'
-        rounded='xl'
-        align='center'
-        justify='center'
-        bgColor={`${category.backgroundColor}`}
-        transition='all 0.2s ease-in-out'
-        _hover={{ opacity: 0.8 }}
-      >
-        <Center p={6}>
-          <Text
-            fontSize={{ base: 28, md: 34 }}
-            color='text.dark'
-            fontWeight='bold'
-            textAlign='center'
-          >
-            {category.categoryName.toUpperCase()}
-          </Text>
-        </Center>
-      </Flex>
-    </>
+    <Flex
+      as={RouterLink}
+      to={`/${category.href}`}
+      maxW={{ base: '100%', md: '385px' }}
+      minW='100%'
+      h='203px'
+      rounded='xl'
+      align='center'
+      justify='center'
+      bgColor={`${category.backgroundColor}`}
+      transition='all 0.2s ease-in-out'
+      _hover={{ opacity: 0.8 }}
+    >
+      <Center p={6}>
+        <Text
+          fontSize={{ base: 28, md: 34 }}
+          color='text.dark'
+          fontWeight='bold'
+          textAlign='center'
+        >
+          {category.categoryName.toUpperCase()}
+        </Text>
+      </Center>
+    </Flex>
   );
 };
