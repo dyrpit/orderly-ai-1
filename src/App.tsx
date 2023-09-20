2a581fed5fd7cd1113ef837d806115f87651c2a0
Rafixx <rafal987.1@o2.pl>
2023-09-20 20:46:47 +0200

    feat: create component for testing Redux

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,19 @@ import { ProductCard } from './components/productCard/ProductCard';
 import { Toaster } from 'react-hot-toast';
 import { CategoryForm } from './components/newCategory/CategoryForm';
 import { Form } from './components/form/LoginAndRegisterForm';
+import TestRedux from './components/test-Redux-functions/testRedux';
 
 function App() {
   return (
     <MainLayout>
-      <Toaster/>
+      <Toaster />
       <Routes>
         <Route path='/' element={<CategoriesList />} />
         <Route path='/:categoryName' element={<ProductsList />} />
         <Route path='/:categoryName/:productName' element={<ProductCard />} />
         <Route path='/addCategory' element={<CategoryForm />} />
         <Route path='/auth' element={<Form />} />
+        <Route path='/test-redux' element={<TestRedux />} />
       </Routes>
     </MainLayout>
   );
