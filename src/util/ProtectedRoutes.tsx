58c69cac2926929302d92fcd0753cc09a5d7a45b
Rafixx <rafal987.1@o2.pl>
2023-09-22 10:08:42 +0200

    refactor: delete protected routes component

--- src/util/ProtectedRoutes.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import AdminPanel from '@components/admin-panel/AdminPanel.tsx';
-
-export const ProtectedRoutes = () => {
-  const token: string | null = sessionStorage.getItem('token');
-  const role: string | null = token !== null ? JSON.parse(token).role : null;
-
-  return token !== null && role === 'admin' ? <AdminPanel /> : <Navigate to='/' />;
-};
