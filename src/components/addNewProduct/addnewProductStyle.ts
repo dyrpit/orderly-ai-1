7b0014c20b84706735aa081a83ed7aef25724f0e
AnnaIdzkowska <121981221+AnnaIdzkowska@users.noreply.github.com>
2023-09-22 13:10:43 +0200

    feat:add styles and validation for component

---
+++ src/components/addNewProduct/addnewProductStyle.ts
@@ -0,0 +1,10 @@
+import { colors } from '../../theme.ts';
+
+export const formLabelStyles = {
+  paddingTop: '10px',
+  fontSize: '20px'
+};
+
+export const inputStyles = {
+  background: '#1E345B',
+};
