a9aaf69adc7745c2e0c27a2ae48ae9e0b217ef2d
Rafixx <rafal987.1@o2.pl>
2023-09-25 09:40:44 +0200

    feat: apply persist for admin Panel reducer

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,17 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import categoriesReducer from './features/categories/categoriesSlice';
 import productsReducer from './features/products/productsSlice';
 import adminPanelReducer from './features/adminPanel/adminPanelSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, adminPanelReducer);
 
 export const store = configureStore({
   reducer: {
     categories: categoriesReducer,
     products: productsReducer,
     // users: usersReducer,
-    adminPanel: adminPanelReducer,
+    adminPanel: persistedReducer,
   },
   devTools: true,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
