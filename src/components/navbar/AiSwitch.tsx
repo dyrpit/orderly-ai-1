aa73a5e63ae34e009d3c11c4074853d53f2cf4eb
Rafixx <rafal987.1@o2.pl>
2023-09-28 21:38:50 +0200

    feat: apply chatGPT logic to AI Switch component

--- src/components/navbar/AiSwitch.tsx
+++ src/components/navbar/AiSwitch.tsx
@@ -1,66 +1,139 @@
-import { useState } from 'react';
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { callGPT } from '@util/api-calls.ts';
+import { TPrompt } from '@/types/prompt.ts';
+import {
+  setIsNotWaitingForResponse,
+  setIsWaitingForResponse,
+  toggleSwitch,
+} from '@/redux/features/gpt/gptSlice';
+import { fetchProducts } from '@/redux/features/products/productsSlice.ts';
+import { fetchCategories } from '@/redux/features/categories/categoriesSlice.ts';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import { StarIcon } from '@chakra-ui/icons';
+import { type TCategory } from '@/types/category';
+import { type TProduct } from '@/types/product';
+import toast from 'react-hot-toast';
 
 export const AiSwitch = () => {
-  const [isSwitchActive, setIsSwitchActive] = useState(false);
+  const dispatch = useAppDispatch();
+  const isWaitingForResponse = useAppSelector(
+    (state) => state.gpt.isWaitingForResponse,
+  );
+  const isSwitchActive = useAppSelector((state) => state.gpt.isSwitchActive);
+
+  let promptGPT: TPrompt[];
 
   const toggleSwitchHandle = () => {
-    isSwitchActive ? setIsSwitchActive(false) : setIsSwitchActive(true);
+    dispatch(toggleSwitch());
+
+    if (!isWaitingForResponse && !isSwitchActive) {
+      console.log('API Call sended');
+      getDataFromOpenAI();
+    }
+  };
+
+  const getDataFromOpenAI = async () => {
+    try {
+      dispatch(setIsWaitingForResponse());
+      const response = await fetch('../../../prompt.json');
+      promptGPT = await response.json();
+
+      const res = await callGPT(promptGPT[0].text);
+      const data = JSON.parse(res.data.choices[0].message.content);
+
+      const { categories, products } = data;
+
+      fetchDataFromAPI({ categories, products });
+
+      await toast.success('Application generated with OpenAI API');
+      dispatch(setIsNotWaitingForResponse());
+      isSwitchActive && dispatch(toggleSwitch());
+    } catch (error) {
+      toast.error('Something went wrong, check console.');
+      dispatch(setIsNotWaitingForResponse());
+      isSwitchActive && dispatch(toggleSwitch());
+      console.log(error);
+    } /* finally {
+      dispatch(setIsNotWaitingForResponse());
+      !isSwitchActive && dispatch(toggleSwitch());
+    } */
+  };
+
+  const fetchDataFromAPI = ({
+    categories,
+    products,
+  }: {
+    categories: TCategory[];
+    products: TProduct[];
+  }) => {
+    dispatch(fetchCategories(categories));
+    dispatch(fetchProducts(products));
   };
 
   return (
-    <Flex
-      w='fit-content'
-      backgroundColor='bg.counter'
-      py={2}
-      px={2}
-      h={10}
-      gap={6}
-      justify='space-between'
-      alignItems='center'
-      borderRadius={40}
-      _hover={{ cursor: 'pointer' }}
-      onClick={toggleSwitchHandle}
-    >
-      <Text
-        color={isSwitchActive ? 'text.primary' : 'text.secondary'}
-        fontSize={{ base: 13, md: 16 }}
-        transition='all 0.2s ease-out'
+    <>
+      <Button
+        onClick={() => {
+          dispatch(setIsNotWaitingForResponse());
+          dispatch(toggleSwitch());
+        }}
       >
-        Build App with chatGPT
-      </Text>
-      <Box
-        position='relative'
-        backgroundColor='bg.secondary'
-        w={20}
-        h={8}
-        p={1}
+        Clear Waiting
+      </Button>
+      <Flex
+        w='fit-content'
+        backgroundColor='bg.counter'
+        py={2}
+        px={2}
+        h={10}
+        gap={6}
+        justify='space-between'
+        alignItems='center'
         borderRadius={40}
-        transition='all 0.2s ease-out'
+        _hover={{ cursor: 'pointer' }}
+        onClick={toggleSwitchHandle}
       >
-        <Flex
-          position='absolute'
-          borderRadius={99}
-          w='fit-content'
-          h='fit-content'
-          justify='center'
-          alignItems='center'
-          backgroundColor='bg.counter'
-          p={1}
-          sx={{
-            transform: isSwitchActive ? 'translateX(200%)' : 'translateX(0px)',
-          }}
+        <Text
+          color={isSwitchActive ? 'text.primary' : 'text.secondary'}
+          fontSize={{ base: 13, md: 16 }}
           transition='all 0.2s ease-out'
         >
-          <StarIcon
-            color={isSwitchActive ? 'text.primary' : 'bg.secondary'}
-            w={4}
-            h={4}
+          Build App with chatGPT
+        </Text>
+        <Box
+          position='relative'
+          backgroundColor='bg.secondary'
+          w={20}
+          h={8}
+          p={1}
+          borderRadius={40}
+          transition='all 0.2s ease-out'
+        >
+          <Flex
+            position='absolute'
+            borderRadius={99}
+            w='fit-content'
+            h='fit-content'
+            justify='center'
+            alignItems='center'
+            backgroundColor='bg.counter'
+            p={1}
+            sx={{
+              transform: isSwitchActive
+                ? 'translateX(200%)'
+                : 'translateX(0px)',
+            }}
             transition='all 0.2s ease-out'
-          />
-        </Flex>
-      </Box>
-    </Flex>
+          >
+            <StarIcon
+              color={isSwitchActive ? 'text.primary' : 'bg.secondary'}
+              w={4}
+              h={4}
+              transition='all 0.2s ease-out'
+            />
+          </Flex>
+        </Box>
+      </Flex>
+    </>
   );
 };
