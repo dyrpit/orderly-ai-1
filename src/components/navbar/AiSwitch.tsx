6839c5e2c5ef0645fc7764d577ce2d8a1f4ff6a8
Rafixx <rafal987.1@o2.pl>
2023-09-28 21:41:33 +0200

    fix: delete test button

--- src/components/navbar/AiSwitch.tsx
+++ src/components/navbar/AiSwitch.tsx
@@ -8,7 +8,7 @@ import {
 } from '@/redux/features/gpt/gptSlice';
 import { fetchProducts } from '@/redux/features/products/productsSlice.ts';
 import { fetchCategories } from '@/redux/features/categories/categoriesSlice.ts';
-import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import { StarIcon } from '@chakra-ui/icons';
 import { type TCategory } from '@/types/category';
 import { type TProduct } from '@/types/product';
@@ -71,69 +71,57 @@ export const AiSwitch = () => {
   };
 
   return (
-    <>
-      <Button
-        onClick={() => {
-          dispatch(setIsNotWaitingForResponse());
-          dispatch(toggleSwitch());
-        }}
+    <Flex
+      w='fit-content'
+      backgroundColor='bg.counter'
+      py={2}
+      px={2}
+      h={10}
+      gap={6}
+      justify='space-between'
+      alignItems='center'
+      borderRadius={40}
+      _hover={{ cursor: 'pointer' }}
+      onClick={toggleSwitchHandle}
+    >
+      <Text
+        color={isSwitchActive ? 'text.primary' : 'text.secondary'}
+        fontSize={{ base: 13, md: 16 }}
+        transition='all 0.2s ease-out'
       >
-        Clear Waiting
-      </Button>
-      <Flex
-        w='fit-content'
-        backgroundColor='bg.counter'
-        py={2}
-        px={2}
-        h={10}
-        gap={6}
-        justify='space-between'
-        alignItems='center'
+        Build App with chatGPT
+      </Text>
+      <Box
+        position='relative'
+        backgroundColor='bg.secondary'
+        w={20}
+        h={8}
+        p={1}
         borderRadius={40}
-        _hover={{ cursor: 'pointer' }}
-        onClick={toggleSwitchHandle}
+        transition='all 0.2s ease-out'
       >
-        <Text
-          color={isSwitchActive ? 'text.primary' : 'text.secondary'}
-          fontSize={{ base: 13, md: 16 }}
-          transition='all 0.2s ease-out'
-        >
-          Build App with chatGPT
-        </Text>
-        <Box
-          position='relative'
-          backgroundColor='bg.secondary'
-          w={20}
-          h={8}
+        <Flex
+          position='absolute'
+          borderRadius={99}
+          w='fit-content'
+          h='fit-content'
+          justify='center'
+          alignItems='center'
+          backgroundColor='bg.counter'
           p={1}
-          borderRadius={40}
+          sx={{
+            transform: isSwitchActive ? 'translateX(200%)' : 'translateX(0px)',
+          }}
           transition='all 0.2s ease-out'
         >
-          <Flex
-            position='absolute'
-            borderRadius={99}
-            w='fit-content'
-            h='fit-content'
-            justify='center'
-            alignItems='center'
-            backgroundColor='bg.counter'
-            p={1}
-            sx={{
-              transform: isSwitchActive
-                ? 'translateX(200%)'
-                : 'translateX(0px)',
-            }}
+          <StarIcon
+            color={isSwitchActive ? 'text.primary' : 'bg.secondary'}
+            w={4}
+            h={4}
             transition='all 0.2s ease-out'
-          >
-            <StarIcon
-              color={isSwitchActive ? 'text.primary' : 'bg.secondary'}
-              w={4}
-              h={4}
-              transition='all 0.2s ease-out'
-            />
-          </Flex>
-        </Box>
-      </Flex>
-    </>
+          />
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
