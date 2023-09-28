bbe600b727dbce69a417bf8472b3466110924ea1
Rafixx <rafal987.1@o2.pl>
2023-09-28 21:36:44 +0200

    feat: add text to spinner

--- src/components/shared/LoadingSpinner.tsx
+++ src/components/shared/LoadingSpinner.tsx
@@ -1,11 +1,26 @@
 import Spinner from '@assets/loading-spinner.svg';
-import { Flex, Image, keyframes } from '@chakra-ui/react';
+import { Flex, Image, Text, keyframes } from '@chakra-ui/react';
 
-const spinAnimation = keyframes({ from: { transform: 'rotate(0deg)' }, to: { transform: 'rotate(360deg)' } });
+const spinAnimation = keyframes({
+  from: { transform: 'rotate(0deg)' },
+  to: { transform: 'rotate(360deg)' },
+});
 export const LoadingSpinner = () => {
   return (
-    <Flex justifyContent='center' alignItems='center' overflow='hidden'>
-      <Image src={Spinner} sx={{ animation: `${spinAnimation} 2s ease infinite` }}></Image>
+    <Flex
+      justifyContent='center'
+      alignItems='center'
+      overflow='hidden'
+      direction='column'
+      gap={12}
+    >
+      <Image
+        src={Spinner}
+        sx={{ animation: `${spinAnimation} 2s ease infinite` }}
+      />
+      <Text fontSize={36} textAlign='center'>
+        Waiting for API Response
+      </Text>
     </Flex>
   );
 };
